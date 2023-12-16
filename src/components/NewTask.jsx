919a1f7a541ad848984240081ae57d8825c957f3
Ahmed Eid <Ahmed_tech67@outlook.com>
2023-12-16 23:53:47 +0200

    Project Code

---
+++ src/components/NewTask.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function NewTasks({ onAdd }) {
+  const [enteredTask, setEnteredTask] = useState("");
+
+  const handleChange = (e) => {
+    setEnteredTask(e.target.value);
+  };
+
+  const handleClick = () => {
+    if (enteredTask.trim() === "") return;
+
+    onAdd(enteredTask);
+    setEnteredTask("");
+  };
+
+  return (
+    <div className="flex itms-center gap-4">
+      <input
+        type="text"
+        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        onChange={handleChange}
+        value={enteredTask}
+      />
+      <button
+        className="text-stone-700 hover:text-stone-950"
+        onClick={handleClick}
+      >
+        Add Task
+      </button>
+    </div>
+  );
+}
